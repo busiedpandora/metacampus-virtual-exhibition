e6d785ad69461e335bac6dcf4f97b87191df0ab8
simone.giamboni <simone.giamboni@student.supsi.ch>
2024-04-10 20:18:06 +0200

    set up unity for android build

--- metacampus2/frontend/MetaCampus2/Assets/Scripts/ResourcesManager.cs
+++ metacampus2/frontend/MetaCampus2/Assets/Scripts/ResourcesManager.cs
@@ -28,8 +28,10 @@ public class ResourcesManager : MonoBehaviour
     private const string metaverseUrlName = "campus-est-supsi";
 
     //private const string hostName = "192.168.45.81"; //hotspot
-    private const string hostName = "10.21.56.224"; //eduroam
+    //private const string hostName = "10.21.56.224"; //eduroam hololens
+    private const string hostName = "10.21.57.147"; //eduroam android
     //private const string hostName = "localhost";
+    //private const string hostName = "10.11.73.241"; //SUPSI
     private const string port = "8080";
     private const string spacesPath = "spaces";
     private string spacesServerUrl = "";
